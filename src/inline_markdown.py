af5aac5a5d3738af5551438ea9010dee1ad50f8b
Krayto <johnjoemly@gmail.com>
2024-05-20 18:07:24 -0300

    added the markdown blocks script & small changes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,9 +1,13 @@
+import re
 from textnode import (
     TextNode,
     text_type_text,
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_code,
+    text_type_link,
+    text_type_image,
 )
 
 
@@ -26,3 +30,64 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        if len(images) == 0:
+            new_nodes.append(node)
+        else:
+            text = node.text
+            for image in images:
+                sections = text.split(f"![{image[0]}]({image[1]})", 1)
+                if sections[0] != "":
+                    new_nodes.append(TextNode(sections[0], text_type_text))
+                new_nodes.append(TextNode(image[0], text_type_image, image[1]))
+                text = sections[-1]
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+        if len(links) == 0:
+            new_nodes.append(node)
+        else:
+            text = node.text
+            for link in links:
+                sections = text.split(f"[{link[0]}]({link[1]})", 1)
+                if sections[0] != "":
+                    new_nodes.append(TextNode(sections[0], text_type_text))
+                new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+                text = sections[-1]
+    return new_nodes
+
+
+def text_to_textnodes(text):
+    original = TextNode(text, text_type_text)
+    new_nodes = []
+    new_nodes.extend(split_nodes_delimiter([original], "**", text_type_bold))
+    new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", text_type_code)
+    new_nodes = split_nodes_link(new_nodes)
+    new_nodes = split_nodes_image(new_nodes)
+    return new_nodes
+
+
+sample = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+
+text_to_textnodes(sample)
--- src/textnode.py
+++ src/textnode.py
@@ -1,42 +1,41 @@
 from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
-    def __init__(self, text, text_type, url = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, target):
-        if self.text == target.text:
-            if self.text_type == target.text_type:
-                if self.url == target.url:
-                    return True
-        return False
+    def __eq__(self, other):
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 
-test_node = TextNode("beans", "bold")
-
 def text_node_to_html_node(text_node):
-    valid_types = ["text", "bold", "italic", "code", "link", "image"]
-    text_type = text_node.text_type
-    if text_type not in valid_types:
-        raise Exception(f"{text_type}: is not a valid text type")
-    if text_type == "text":
-        return LeafNode(value= text_node.text)
-    elif text_type == "bold":
-        return LeafNode(tag="b", value= text_node.text)
-    elif text_type == "italic":
-        return LeafNode(tag="i", value= text_node.text)
-    elif text_type == "code":
-        return LeafNode(tag="code", value= text_node.text)
-    elif text_type == "link":
-        link = text_node.url
-        return LeafNode(tag="a", value= text_node.text, props= {"href":link})
-    elif text_type == "image":
-        link = text_node.url
-        return LeafNode(tag="img", props= {"src":link, "alt":text_node.text})
-    
-
-print(text_node_to_html_node(test_node))
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
